7d6441e374f381f184fdb7b9dc5a410d64d2b150
Oskar Gawlak <oskar.gawlak@gmail.com>
2017-05-14 21:09:42 +0200

    Make app intro

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  { text: 'Welcome to RestaurantsApp', color: '#03A9F4' },
+  { text: 'Use this to find a restaurant', color: '#009688' },
+  { text: 'Set your location, then swipe away', color: '#03A9F4' }
+];
 
 class WelcomeScreen extends Component {
+  onSlidesComplete = () => {
+    this.props.navigation.navigate('auth');
+  }
+
   render() {
     return (
-      <View>
-        <Text>Welcome Screen</Text>
-        <Text>Welcome Screen</Text>
-        <Text>Welcome Screen</Text>
-        <Text>Welcome Screen</Text>
-        <Text>Welcome Screen</Text>
-        <Text>Welcome Screen</Text>
-        <Text>Welcome Screen</Text>
-        <Text>Welcome Screen</Text>
-      </View>
+      <Slides
+        data={SLIDE_DATA}
+        onComplete={this.onSlidesComplete}
+      />
     );
   }
 }
