48ec4f8c95749c7e20dacc45bb0a3a697955a9aa
Oskar Gawlak <oskar.gawlak@gmail.com>
2017-05-14 14:10:26 +0200

    Add React Navigation

---
+++ main.js
@@ -0,0 +1,60 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import AuthScreen from './screens/AuthScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+class App extends React.Component {
+  render() {
+    const MainNavigator = TabNavigator(
+      {
+        welcome: { screen: WelcomeScreen },
+        auth: { screen: AuthScreen },
+        main: {
+          screen: TabNavigator(
+            {
+              map: { screen: MapScreen },
+              deck: { screen: DeckScreen },
+              review: {
+                screen: StackNavigator(
+                  {
+                    review: { screen: ReviewScreen },
+                    settings: { screen: SettingsScreen }
+                  }
+                )
+              }
+            },
+            {
+              tabBarPosition: 'bottom',
+              swipeEnabled: false,
+              animationEnabled: false
+            }
+          )
+        }
+      },
+      {
+        tabBarPosition: 'bottom',
+      }
+    );
+    return (
+      <View style={styles.container}>
+        <MainNavigator />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === 'android' ? 24 : 0
+  },
+});
+
+Expo.registerRootComponent(App);
---
+++ screens/ReviewScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ReviewScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Review Screen</Text>
+        <Text>Review Screen</Text>
+        <Text>Review Screen</Text>
+        <Text>Review Screen</Text>
+        <Text>Review Screen</Text>
+        <Text>Review Screen</Text>
+        <Text>Review Screen</Text>
+        <Text>Review Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default ReviewScreen;
